6a8d01f0db222d6afda1c232312ba8af2c8a7768
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-05-01 19:21:35 +0200

    feat!: add lsp and completion

--- lua/completion.lua
+++ lua/completion.lua
@@ -5,3 +5,10 @@
 vim.g.coq_settings = {
   xdg = true,
 }
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserCoqAttach', {}),
+  callback = function(ev)
+    require('coq')
+    vim.cmd([[:COQnow --shut-up]])
+  end
+})
