67e2da77227bfdce97115fbbd871b58fc91134c0
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-05-09 19:54:10 +0200

    fix: clean swap command

--- lua/commands.lua
+++ lua/commands.lua
@@ -4,7 +4,7 @@
 
 -- Swap folder
 vim.cmd("command! ListSwap split | enew | r !ls -l " .. Nvim_tmp_directory)
-vim.cmd("command! CleanSwap !rm -rf " .. Nvim_tmp_directory)
+vim.cmd("command! CleanSwap !rm -rf " .. Nvim_tmp_directory .. "/*.swp")
 
 -- Open help tags
 vim.cmd("command! HelpTags Telescope help_tags")
