c0316a6ebc824b3ce159f15ebcf8653f9aa89113
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-06-23 14:26:04 +0200

    fix: stylelint package

--- packages/lspservers/default.nix
+++ packages/lspservers/default.nix
@@ -1,12 +1,10 @@
-{ pkgs, lib, nodejs, stdenv }:
+{ pkgs, nodejs, stdenv }:
 
 let
-  inherit (lib) makeExtensible extends;
-
   nodePackages = final: import ./composition.nix {
     inherit pkgs nodejs;
     inherit (stdenv.hostPlatform) system;
   };
 in
-  makeExtensible (extends nodePackages)
+  nodePackages
 
