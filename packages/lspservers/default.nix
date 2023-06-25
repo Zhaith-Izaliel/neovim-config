76040716c40fe9d5e32bd00f4092028dbb0089e6
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-06-25 17:09:16 +0200

    fix: attempting fix commitlint

--- packages/lspservers/default.nix
+++ packages/lspservers/default.nix
@@ -5,7 +5,10 @@ let
     inherit pkgs stdenv;
     nodejs = pkgs.nodejs;
   });
-  nodeDependencies = (pkgs.callPackage nodejs-servers).nodeDependencies;
+  nodeDependencies = (pkgs.callPackage import ./nodejs {
+    inherit pkgs stdenv;
+    nodejs = pkgs.nodejs;
+  }).nodeDependencies;
   overriden-commitlint = nodejs-servers."@commitlint/cli".overrideAttrs(final: prev: {
     buildPhase = ''
       ln -s ${nodeDependencies}/lib/node_modules ./node_modules
