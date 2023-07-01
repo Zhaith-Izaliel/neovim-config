a068746c23d39f3aa456b83712ea9d3821fa35e2
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-07-01 18:30:43 +0200

    fix: commitlint

--- packages/lspservers/default.nix
+++ packages/lspservers/default.nix
@@ -15,7 +15,7 @@ let
     mkdir -p $out/node_modules
 
     ln -s ${nodejs-servers.commitlint-format-json}/lib/node_modules/* $out/node_modules
-    ln -s ${nodejs-servers."@commitlint/config-conventional"}/lib/node_modules/* $out/node_modules
+    ln -s ${nodejs-servers."@commitlint/config-conventional"}/lib/node_modules/* $out/lib/node_modules
     '';
   });
 in
