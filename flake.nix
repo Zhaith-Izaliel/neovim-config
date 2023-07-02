d6d19780217e86e5bb180dcd2dce26f42f171bac
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-07-03 01:11:11 +0200

    fix: haskell-tools-nvim

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
         # Doc Here:
         # https://github.com/NixOS/nixpkgs/blob/nixos-22.11/doc/languages-frameworks/vim.section.md
         programs.neovim = {
-          plugins = plugins ++ [ haskell-tools-nvim.packages.default ];
+          plugins = plugins ++ [ inputs.haskell-tools-nvim.packages.default ];
           enable = true;
           withNodeJs = true;
           withPython3 = true;
