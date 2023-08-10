d40f60517fcf3d511d37d95ee6395ba8163a6130
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-08-10 11:08:50 +0200

    fix: attempting something

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,26 +1,26 @@
 -- Hardtime
-require('hardtime').setup {
+require("hardtime").setup {
   allow_different_key = true,
   enabled = true;
   restricted_keys = {
-    ['<UP>'] = { 'n', 'x' },
-    ['<DOWN>'] = { 'n', 'x' },
-    ['<LEFT>'] = { 'n', 'x' },
-    ['<RIGHT>'] = {'n', 'x' },
-    ['+'] = {},
-    ['-'] = {},
-    ['g<UP>'] = { 'n', 'x' },
-    ['g<DOWN>'] = { 'n', 'x' },
-    ['<CR>'] = { 'n', 'x' },
-    ['<C-M>'] = { 'n', 'x' },
-    ['<C-N>'] = { 'n', 'x' },
-    ['<C-P>'] = { 'n', 'x' },
+    ["<UP>"] = { "n", "x" },
+    ["<DOWN>"] = { "n", "x" },
+    ["<LEFT>"] = { "n", "x" },
+    ["<RIGHT>"] = {"n", "x" },
+    ["+"] = {},
+    ["-"] = {},
+    ["g<UP>"] = { "n", "x" },
+    ["g<DOWN>"] = { "n", "x" },
+    ["<CR>"] = { "n", "x" },
+    ["<C-M>"] = { "n", "x" },
+    ["<C-N>"] = { "n", "x" },
+    ["<C-P>"] = { "n", "x" },
   },
   disabled_keys = {
-    ['<UP>'] = { 'i' },
-    ['<DOWN>'] = { 'i' },
-    ['<LEFT>'] = { 'i' },
-    ['<RIGHT>'] = { 'i' },
+    ["<UP>"] = { "i" },
+    ["<DOWN>"] = { "i" },
+    ["<LEFT>"] = { "i" },
+    ["<RIGHT>"] = { "i" },
   },
 }
 
