902cdd9f45c6dc1e3ed359d41d068bd254f1295b
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-08-19 21:41:35 +0200

    feat: move module in its own file

--- flake.nix
+++ flake.nix
@@ -18,75 +18,9 @@ in
     nil.url = "github:oxalica/nil";
   } // plugins;
 
-  outputs = inputs @ { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-    localLib = import ./lib { inherit pkgs; lib = nixpkgs.lib; };
-  in
-  with nixpkgs.lib;
+  outputs = { self, nixpkgs, ... }:
   {
-    nixosModules.default = { config, lib, stdenv, ... }:
-    let
-      cfg = config.programs.neovim.zhaith-config;
-      configPackage = pkgs.stdenv.mkDerivation {
-        name = "zhaith-neovim-config";
-        src = "${self}";
-        installPhase = ''
-        mkdir -p $out
-        cp -r --target-directory $out lua init.lua
-        '';
-      };
-      init = builtins.readFile "${configPackage}/init.lua";
-      lua = lib.sources.cleanSource "${configPackage}/lua";
-      dependencies = (import ./dependencies { inherit pkgs stdenv; });
-      oldPlugins = (import ./plugins { inherit pkgs lib; });
-    in
-    {
-      options = {
-        programs.neovim.zhaith-config = {
-          enable = mkOption {
-            default = false;
-            example = true;
-            type = types.bool;
-            description = ''
-            Enable Zhaith's Neovim config
-            '';
-          };
-        };
-      };
-      config = mkIf cfg.enable (mkMerge [{
-        home.file = {
-          ".config/nvim/lua".source = lua; # Import config
-          ".commitlintrc.js".text = dependencies.files.commitlintrc;
-        };
-
-        nixpkgs.overlays = [
-          inputs.haskell-tools-nvim.overlays.default
-          inputs.nil.overlays.default
-          (final: prev: import ./overlay { inherit final prev; })
-        ];
-
-        # Doc Here:
-        # https://github.com/NixOS/nixpkgs/blob/nixos-22.11/doc/languages-frameworks/vim.section.md
-        programs.neovim = {
-          plugins = oldPlugins ++ localLib.pluginsToList plugins;
-          enable = true;
-          withNodeJs = true;
-          withPython3 = true;
-          vimAlias = true;
-          viAlias = true;
-          vimdiffAlias = true;
-          extraPackages = dependencies.packages;
-          extraLuaConfig = ''
-
-          omnisharp_path = "${pkgs.omnisharp-roslyn}/lib/omnisharp-roslyn/OmniSharp.dll"
-          '' + init;
-        };
-      }]);
-    };
+    nixosModules.default = import ./module { inherit self; };
   };
 }
 
