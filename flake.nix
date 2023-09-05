9b840b0da329bfac093e7c1042e007431d5fc85d
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-05 02:48:57 +0200

    fix: remove useless inputs

--- flake.nix
+++ flake.nix
@@ -3,16 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     haskell-tools-nvim = {
       url = "github:mrcjkb/haskell-tools.nvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # Vim plugins
+    # Neovim plugins
     nvim-neogen = {
       url = "github:danymat/neogen";
       flake = false;
