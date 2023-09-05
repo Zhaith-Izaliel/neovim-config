d41b3764cf6c9bbf5349712523c777804c6647a4
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-06 01:10:38 +0200

    fix: ltex being extremely annoying with noice

--- lua/lsp/ltex.lua
+++ lua/lsp/ltex.lua
@@ -5,15 +5,8 @@ require('lspconfig').ltex.setup {
   settings = {
     ltex = {
       language = 'en-US',
-      -- checkFrequency = 'save',
+      checkFrequency = 'save',
     },
   },
-  handlers = {
-    ['textDocument/publishDiagnostics'] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        update_in_insert = false,
-      }
-    ),
-  }
 }
 
