a4587245e8b786fc72b2479d7b7e7a8f7410e39d
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-05 19:23:19 +0200

    fix: noice

--- lua/ui/ui.lua
+++ lua/ui/ui.lua
@@ -1,4 +1,9 @@
 require("noice").setup({
+  cmdline = {
+    format = {
+      cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+    },
+  },
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
