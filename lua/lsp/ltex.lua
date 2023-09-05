baec4ce11d6fbe46a53b3f1c6f99c3a74facc89e
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-06 00:20:49 +0200

    fix: trying to fix ltex

--- lua/lsp/ltex.lua
+++ lua/lsp/ltex.lua
@@ -7,12 +7,5 @@ require('lspconfig').ltex.setup {
       language = 'en-US',
     },
   },
-  handlers = {
-    ['textDocument/publishDiagnostics'] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-        update_in_insert = false,
-      }
-    ),
-  }
 }
 
