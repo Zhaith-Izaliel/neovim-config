9ce4d60687cd897f26500feef6054a2212220d89
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-06 01:24:55 +0200

    fix: volar

--- lua/lsp/typescript.lua
+++ lua/lsp/typescript.lua
@@ -1,8 +1,24 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local is_npm_package_installed = require('utils').is_npm_package_installed
 
 -- Typescript
 require('typescript-tools').setup {
   capabilities = capabilities,
+  filetypes = is_npm_package_installed 'vue'
+    and {
+      "javascript",
+      "javascriptreact",
+      "javascript.jsx",
+      "typescript",
+      "typescriptreact",
+      "typescript.tsx"
+    }
+    or {
+      "javascriptreact",
+      "javascript.jsx",
+      "typescriptreact",
+      "typescript.tsx"
+    },
 
 }
 
