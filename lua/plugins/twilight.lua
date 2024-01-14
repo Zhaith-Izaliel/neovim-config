bb48d313cf53054b0c88428273184b1457d5c222
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-14 15:51:16 +0100

    fix(twilight): mapping

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -1,5 +1,13 @@
 local Utils = require("utils")
+local macchiato = require("catppuccin.palettes").get_palette "macchiato"
 local nnoremap = Utils.nnoremap
 
-require("twilight").setup {}
-nnoremap("<leader>t", require("twilight").toggle(), "Twilight: toggle")
+require("twilight").setup {
+	dimming = {
+		alpha = 0.25, -- amount of dimming
+		-- we try to get the foreground from the highlight groups or fallback color
+		color = { "Normal", macchiato.text },
+		term_bg = macchiato.base,
+	},
+}
+nnoremap("<leader>t", require("twilight").toggle, "Twilight: toggle")
