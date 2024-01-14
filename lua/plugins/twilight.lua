6401eb8e6887459b96323a9f0a9295a35a819ad4
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-14 15:54:35 +0100

    fix(twilight): dimming value

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -4,7 +4,7 @@ local nnoremap = Utils.nnoremap
 
 require("twilight").setup {
 	dimming = {
-		alpha = 0.25, -- amount of dimming
+		alpha = 0.15, -- amount of dimming
 		-- we try to get the foreground from the highlight groups or fallback color
 		color = { "Normal", macchiato.text },
 		term_bg = macchiato.base,
