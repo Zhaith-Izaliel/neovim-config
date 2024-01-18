51e4c9262ee751dd02b3a315b5107d53a8c211e0
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-18 11:24:38 +0100

    fix(lsp): autoformatting with tsserver

--- lua/lsp/base.lua
+++ lua/lsp/base.lua
@@ -22,17 +22,22 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     nnoremap('gD', vim.lsp.buf.declaration, 'LSP: Go to declaration.', opts)
     nnoremap('gd', vim.lsp.buf.definition, 'LSP: Go to definition.', opts)
-    nnoremap('K', vim.lsp.buf.hover, 'LSP: Displays hover information about the symbol under the cursor in a floating window. Calling the function twice will jump into the floating window.', opts)
+    nnoremap('K', vim.lsp.buf.hover,
+      'LSP: Displays hover information about the symbol under the cursor in a floating window. Calling the function twice will jump into the floating window.',
+      opts)
     nnoremap('gi', vim.lsp.buf.implementation, 'LSP: Go to implementation.', opts)
-    nnoremap('<C-k>', vim.lsp.buf.signature_help, 'LSP: Displays signature information about the symbol under the cursor in a floating window.', opts)
-    nnoremap('<leader>wa', vim.lsp.buf.add_workspace_folder, 'LSP: Add another project root (workspace) to work in, in the current session.', opts)
+    nnoremap('<C-k>', vim.lsp.buf.signature_help,
+      'LSP: Displays signature information about the symbol under the cursor in a floating window.', opts)
+    nnoremap('<leader>wa', vim.lsp.buf.add_workspace_folder,
+      'LSP: Add another project root (workspace) to work in, in the current session.', opts)
     nnoremap('<leader>wr', vim.lsp.buf.remove_workspace_folder, 'LSP: Remove a workspace from the current session.', opts)
     nnoremap('<leader>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, 'LSP: List workspaces.', opts)
     nnoremap('<leader>D', vim.lsp.buf.type_definition, 'LSP: Go to type definition.', opts)
     nnoremap('<leader>rn', vim.lsp.buf.rename, 'LSP: Rename the token under the cursor.', opts)
-    noremap({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, 'LSP: Select a code action available at the current cursor position.', opts)
+    noremap({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action,
+      'LSP: Select a code action available at the current cursor position.', opts)
     nnoremap('gr', vim.lsp.buf.references, 'LSP: Get references of the token under the cursor.', opts)
     nnoremap('<leader>ft', function()
       vim.lsp.buf.format(Utils.get_lsp_format_options())
@@ -41,9 +46,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.diagnostic.config({
-    virtual_text = true,
-    update_in_insert = false,
-    underline = true,
-    signs = true,          -- Keep gutter signs
+  virtual_text = true,
+  update_in_insert = false,
+  underline = true,
+  signs = true, -- Keep gutter signs
 })
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('LSPBufFormatting', {}),
+  callback = function()
+    vim.lsp.buf.format(Utils.get_lsp_format_options())
+  end,
+})
