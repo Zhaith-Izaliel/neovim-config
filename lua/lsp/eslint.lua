3f8a241b0a27c0ed3481de4a75f6a570d554db2d
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-24 11:17:38 +0100

    feat(eslint)!: move to eslint_d

--- lua/lsp/eslint.lua
+++ lua/lsp/eslint.lua
@@ -3,6 +3,7 @@ local capabilities = require('lsp.utils').capabilities
 
 require('lspconfig').eslint.setup { -- JS/TS
   capabilities = capabilities,
+  cmd = { 'eslint_d', '--fix-to-stdout', '--stdin', '--stdin-filename', '$FILENAME' },
   on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd('BufWritePre', {
       buffer = bufnr,
@@ -10,4 +11,3 @@ require('lspconfig').eslint.setup { -- JS/TS
     })
   end,
 }
-
