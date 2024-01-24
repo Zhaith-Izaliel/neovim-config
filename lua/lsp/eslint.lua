54cfbfb38ef49b0c919e04e192aa62eb02f13020
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-24 11:29:14 +0100

    fix(eslint): revert to default eslint temporarly

--- lua/lsp/eslint.lua
+++ lua/lsp/eslint.lua
@@ -3,7 +3,7 @@ local capabilities = require('lsp.utils').capabilities
 
 require('lspconfig').eslint.setup { -- JS/TS
   capabilities = capabilities,
-  cmd = { 'eslint_d', '--fix-to-stdout', '--stdin', '--stdin-filename', '$FILENAME' },
+  -- cmd = { 'eslint_d', '--fix-to-stdout', '--stdin', '--stdin-filename', '$FILENAME' },
   on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd('BufWritePre', {
       buffer = bufnr,
