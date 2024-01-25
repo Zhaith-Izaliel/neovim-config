8c46faf0684219e8eef52d96cf5c11d5183bec0a
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-25 15:24:41 +0100

    fix: eslint_d failing without config file

--- lua/formatter.lua
+++ lua/formatter.lua
@@ -1,20 +1,29 @@
 -- ---------------
 -- ---Formatter---
 -- ---------------
-require("conform").setup({
+require('conform').setup({
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
   formatters_by_ft = {
-    lua = { "stylua" },
-    json = { "jq" },
+    lua = { 'stylua' },
+    json = { 'jq' },
     -- Use a sub-list to run only the first available formatter
-    javascript = { { "eslint_d", "prettierd", "prettier" } },
-    typescript = { { "eslint_d", "prettierd", "prettier" } },
-    vue = { { "eslint_d", "prettierd", "prettier" } },
-    markdown = { "mdformat" },
-    bash = { "shellcheck" },
+    javascript = { { 'eslint_d', 'prettierd', 'prettier' } },
+    typescript = { { 'eslint_d', 'prettierd', 'prettier' } },
+    vue = { { 'eslint_d', 'prettierd', 'prettier' } },
+    markdown = { 'mdformat' },
+    bash = { 'shellcheck' },
   },
 })
+
+require('conform.formatters.eslint_d').cwd = require('conform.util').root_file({
+  '.eslint.js',
+  '.eslint.cjs',
+  '.eslint.yaml',
+  '.eslint.yml',
+  '.eslint.json',
+})
+require('conform.formatters.eslint_d').require_cwd = true
