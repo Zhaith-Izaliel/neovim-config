b826bddf0fa38fa2e078ffa839b0b1ecb27c56a9
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2024-01-25 14:48:18 +0100

    fix: tailwind running when not installed

--- lua/lsp/tailwindcss.lua
+++ lua/lsp/tailwindcss.lua
@@ -1,6 +1,46 @@
 local capabilities = require('lsp.utils').capabilities
+local is_npm_package_installed = require('utils').is_npm_package_installed
+
+local function get_filetypes()
+  if is_npm_package_installed('tailwindcss') then
+    return {
+      "astro",
+      "astro-markdown",
+      "django-html",
+      "htmldjango",
+      "edge",
+      "eelixir",
+      "elixir",
+      "ejs",
+      "erb",
+      "eruby",
+      "gohtml",
+      "gohtmltmpl",
+      "haml",
+      "handlebars",
+      "hbs",
+      "html",
+      "html-eex",
+      "markdown",
+      "mdx",
+      "css",
+      "less",
+      "postcss",
+      "sass",
+      "scss",
+      "stylus",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "svelte"
+    }
+  end
+  return {}
+end
 
 require('lspconfig').tailwindcss.setup {
   capabilities = capabilities,
+  filetypes = get_filetypes(),
 } -- TailwindCSS
-
